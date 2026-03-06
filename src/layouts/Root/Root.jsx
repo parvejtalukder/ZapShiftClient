c970b9d3730364a2fc801b324518e0464112e11f
parvejtalukder <mail@parvejhusentalukder.com>
2026-03-06 12:42:32 +0600

    Update: GitHub, ReactRouter, Tailwind, DaisyUI setted Up.

---
+++ src/layouts/Root/Root.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Outlet } from 'react-router';
+import Header from '../../utility/Header/Header';
+import Footer from '../../utility/Footer/Footer';
+
+const Root = () => {
+    return (
+        <div>
+            <Header></Header>
+            <Outlet></Outlet>
+            <Footer></Footer>
+        </div>
+    );
+};
+
+export default Root;
